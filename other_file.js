b85c222a87d0a218c232b6aac4b9904031f055fc
tami winn <uthippy@gmail.com>
2017-12-06 23:55:54 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,15 +1,1 @@
-/home/jmburges/code/labs/js-functions-lab/test.js:1                                     
-(function (exports, require, module, __filename, __dirname) { console.log(name)         
-  var index = require("./index.js")                                                                        ^             
  
-ReferenceError: name is not defined                                                     
-    at Object.<anonymous> (/home/jmburges/code/labs/js-functions-lab/test.js:1:75)      
-    at Module._compile (module.js:570:32)                                               
-    at Object.Module._extensions..js (module.js:579:10)                                 
-    at Module.load (module.js:487:32)                                                   
-    at tryModuleLoad (module.js:446:12)                                                 
-    at Function.Module._load (module.js:438:3)                                          
-    at Module.runMain (module.js:604:10)                                                
-    at run (bootstrap_node.js:393:7)                                                    
-    at startup (bootstrap_node.js:150:9)                                                
-    at bootstrap_node.js:508:3       
